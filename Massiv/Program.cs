138f90679188a528048f41202de0c2deca0c4b00
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-07-03 07:43:44 +0500

    Add project files.

---
+++ Massiv/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Massiv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massiv = new int[] { 1, 2, 5, 4, 7, 8, 96, 3, 1 };
+
+            int result = getMaxNumber(massiv);
+
+            Console.WriteLine(result);
+        }
+        static int getMaxNumber(int[] massiv) ///
+        {
+            int maxNumber = massiv[1];
+
+            for (int i = 0; i < massiv.Length; i++)
+            {
+                if (i % 2 != 0 && massiv[i] > maxNumber)
+                {
+                    maxNumber = massiv[i];
+                }
+            }
+            return maxNumber;
+        }
+    }
+}
